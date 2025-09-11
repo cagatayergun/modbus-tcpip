23eeca5dcf8cce40bdceae10c4a43e9c5f79d4bb
Çağatay ERGÜN <cagatay.ergunkou2@outlook.com>
2025-09-12 00:23:14 +0300

    v0.3.18

---
+++ TekstilScada.Web/Models/ChartData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace TekstilScada.Web.Models
+{
+    public class ChartData
+    {
+        public List<string> Labels { get; set; }
+        public List<double> Values { get; set; }
+        public string Color { get; set; }
+    }
+}
---
+++ TekstilScada.Web/Models/KpiViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TekstilScada.Web.Models
+{
+    public class KpiViewModel
+    {
+        public string Title { get; set; }
+        public string Value { get; set; }
+        public string Color { get; set; }
+    }
+}
