5c2b979fc073bc05adae090a5caff63d6d11d29e
Çağatay ERGÜN <cagatay.ergunkou2@outlook.com>
2025-10-27 15:36:20 +0300

    v1.0.29

--- TekstilScada.WebAPI/Program.cs
+++ TekstilScada.WebAPI/Program.cs
@@ -30,6 +30,7 @@ builder.Services.AddSingleton<DashboardRepository>();
 builder.Services.AddSingleton<PlcPollingService>();
 builder.Services.AddSingleton<SignalRBridgeService>();
 builder.Services.AddSingleton<FtpTransferService>();
+builder.Services.AddSingleton<RecipeConfigurationRepository>();
 // PLC Polling servisini arka planda çalýþacak bir hizmet olarak ekliyoruz.
 builder.Services.AddHostedService<PlcPollingBackgroundService>();
 //builder.Services.AddScoped<FtpService>();
